cf2f8447153e18faa79a98de85c1ad4c7c3f4655
Hugo Sodré <hugosodre031@gmail.com>
2022-07-25 18:09:46 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+var botao = document.getElementById('botao')
+
+var quantidadeCol;
+var quantidadeLin;
+var seletores = document.getElementById('seletor').value
+
+function gerarDiv(){
+    for(var i = 1; i = seletores; i++){
+              
+        for(var j = 0; j = seletores; j++){
+            quantidadeCol = seletores /2;
+            var criaDiv = document.createElement('div')
+            document.body.appendChild(criaDiv)
+        }
+    } 
+         
+}
