f0ef15ad7d8656294b3ada6654ea1cc32a8e96f1
Hugo Sodré <hugosodre031@gmail.com>
2022-07-27 16:08:56 -0300

    função gerar div pronta!
    função que gera as divs dinamicamentes pronta

--- script.js
+++ script.js
@@ -1,40 +1,33 @@
-const body = document.body;
-var contador = contador + 1;
-
-
 
 function geraDiv() {
     var seletores = document.getElementById("seletor");
     var option = seletores.options[seletores.selectedIndex];
     console.log(option.value);
     
-    // 
-    let div = document.createElement("div");
-        div.id = '1';
-        div.className = 'pai'
-        div.classList.add("divs");
-
-        
-
-    // 
     var valor = option.value;
     var v2 = valor /2;
-    for (let i = 0; i < 2; i++) {
-        
-        body.append(div);
-        for (let j = 1; j < v2; j++) {
-            
-            let div2= document.createElement("div");
-            div2.classList.add("divs");
-            body.appendChild(div2);
-            console.log(div2); 
-        }
-        // }
-        
-    }
-}
-// let div = document.createElement("div");
-// div.classList.add("divs");
 
-// body.appendChild(div);
-// console.log(div); 
+    var divisoes = document.getElementById('geral')    
+    
+    var conteudo = "";
+    var cont = 1;
+    
+    // For Linha
+    
+    for(var i = 0; i < 2 ; i++){
+        conteudo += '<div class="linha divs2">'
+        // For colunas
+        for(var j = 0; j < v2; j++){
+            conteudo += '<div class="celula divs2" id="'+cont+'"></div>'
+            cont++
+        }
+        conteudo += '</div>'
+        divisoes.innerHTML = conteudo
+    }
+
+    console.log(conteudo)
+}
+
+function embaralha(){
+    
+}
